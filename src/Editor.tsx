f9bb8b946bd33f2e755d53c42dc4ab788f8318f3
Masaki Hara <ackie.h.gmai@gmail.com>
2022-10-22 08:14:06 +0900

    Add descriptions

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,11 +1,22 @@
 import React, { useReducer } from "react";
 import { initialState, reducer } from "./state";
 import { BitmapEditor } from "./BitmapEditor";
+import finderPatternImg from "./finder-pattern.png";
+import timingPatternImg from "./timing-pattern.png";
 
 export const Editor: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
+      {
+        state.step === "EditFP"
+        ? (
+          <h2>Step 1: paint the finder patterns</h2>
+        )
+        : (
+          <h2>Step 2: paint the timing patterns</h2>
+        )
+      }
       <div>
         <button
           onClick={() => {
@@ -35,6 +46,31 @@ export const Editor: React.FC = () => {
         </button>
       </div>
       <BitmapEditor state={state} dispatch={dispatch} />
+      <h2>Advice</h2>
+      {
+        state.step === "EditFP"
+        ? (
+          <>
+            <p>
+              <strong>Finder patterns</strong> are the distinctive nested squares.
+            </p>
+            <img src={finderPatternImg} alt="Figure: finder pattern" />
+          </>
+        )
+        : (
+          <>
+            <p>
+              <strong>Timing patterns</strong> are stripes usually hidden in
+              the code, and are used to adjust rows and columns.
+            </p>
+            <p>
+              In regular QR codes, they connect the three finder patterns
+              like bridges.
+            </p>
+            <img src={timingPatternImg} alt="Figure: timing pattern" />
+          </>
+        )
+      }
     </>
   );
 };
